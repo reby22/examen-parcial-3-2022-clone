1358655af89da3768ea4965dc9843b80b39140fe
reby22 <ramf2298@gmail.com>
2022-05-05 15:07:55 -0500

    Commit examen corregido

--- src/main/java/edu/uaslp/objetos/shoppingcart/ShoppingCart.java
+++ src/main/java/edu/uaslp/objetos/shoppingcart/ShoppingCart.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class ShoppingCart {
     private List<Item> items = new ArrayList<>();
+    private int cont;
 
     public BigDecimal getTotalCost() {
         if(isEmpty()){
@@ -20,6 +21,12 @@ public class ShoppingCart {
     }
 
     public void addItem(Item item) {
+        for(Item itemAd: items){
+            if(itemAd.getCode() == item.getCode() && itemAd.getUnitCost().compareTo(item.getUnitCost())==0){
+                itemAd.setQuantity(itemAd.getQuantity()+item.getQuantity());
+                return;
+            }
+        }
         items.add(item);
     }
 
@@ -33,15 +40,26 @@ public class ShoppingCart {
         }
     }
 
-    public boolean getItemsCount() {
-        return false;
+    public int getItemsCount() {
+
+        for(int i=0; i<items.size(); i++){
+            cont= cont + items.get(i).getQuantity();
+        }
+        return cont;
     }
 
     public List<Item> getItems() {
         return items;
     }
 
-    public void removeItem(String itemCode2) {
-        items.remove(itemCode2);
+    public void removeItem(String itemCode) {
+        for(Item itemremove: items){
+            if(itemremove.getCode()==itemCode){
+                itemremove.setQuantity(itemremove.getQuantity()-1);
+                if(itemremove.getQuantity()==0)
+                    items.remove(itemremove);
+                return;
+            }
+        }
     }
 }
